4fc65e7018a563bfbfc8175ec2bce92015e23fd2
Filipe Oliveira <filipeifrscanoas@gmail.com>
2017-06-01 19:10:57 -0300

    fix: Controle de Schema na SchemaObservable

--- database/migrations/2017_05_29_153318_create_pessoas_table.php
+++ database/migrations/2017_05_29_153318_create_pessoas_table.php
@@ -14,7 +14,7 @@ class CreatePessoasTable extends Migration
     public function up()
     {
          Schema::create('pessoas', function (Blueprint $table) {
-             $table->increments('id');
+            $table->increments('id');
             $table->string('nome');
             $table->string('email')->unique();
             $table->string('senha');
