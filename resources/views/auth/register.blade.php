eb79df232b846084d291cefb3082d2a5e6ca6df4
rafaellabueno <rafaellasbueno@gmail.com>
2017-06-27 11:36:30 -0300

    feat: Tela de cadastro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,104 +1,143 @@
 @extends('layouts.app')
 
+@section('css')
+<link href="{{ asset('css/register.css') }}" rel="stylesheet">
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('nome') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="nome" value="{{ old('nome') }}" required autofocus>
-
-                                @if ($errors->has('nome'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('nome') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+<div class="wrapper">
+    <div class="container" style="width: 700px; height: 750px; background-color: #eeeeee; border-radius: 10px 20px;">
+        <div class="row">
+            <div class="col-md-12 col-lg-12">
+                <center><h2>Cadastro de Participante</h2></center>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-lg-10 col-lg-offset-1">
+                @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <div class="container-fluid">
+                        <div class="alert-icon">
+                            <i class="material-icons">error_outline</i>
                         </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        
-                        <div class="form-group{{ $errors->has('cpf') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">CPF</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="cpf" value="{{ old('cpf') }}" required autofocus>
-
-                                @if ($errors->has('cpf'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('cpf') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        
-                           <div class="form-group{{ $errors->has('dt_nascimento') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Nascimento</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="date" class="form-control" name="dt_nascimento" value="{{ old('dt_nascimento') }}" required autofocus>
-
-                                @if ($errors->has('dt_nascimento'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('dt_nascimento') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('senha') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="senha" required>
-
-                                @if ($errors->has('senha'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('senha') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="senha_confirmation" required>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true"><i class="material-icons">clear</i></span>
+                        </button>
+                        <b>Ops! </b> Há erros em seu cadastro:
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
             </div>
+            @endif
+            <div class="alert alert-warning">
+                <div class="container-fluid">
+                    <div class="alert-icon">
+                        <i class="material-icons">warning</i>
+                    </div>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true"><i class="material-icons">clear</i></span>
+                    </button>
+                    <b>ATENÇÃO: </b>É de total resposabilidade do usuário a veracidade dos dados aqui informados, pois estes serão utilizados para a emissão de certificados
+                </div>
+            </div>
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
+                <div class="content">
+                            {{ csrf_field() }}
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="material-icons">face</i>
+                        </span>
+                        
+                        <input type="text" id="name" class="form-control" name="name" value="{{ old('name') }}" placeholder=" Nome">
+                    </div>
+
+                    <br>
+                    <div class="col-md-12 col-md-offset-1">
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <i class="material-icons">credit_card</i>
+                            </span>
+                            <input type="cpf" id="cpf" maxlength="14" OnKeyPress="formatar('###.###.###-##', this)" class="form-control" name="cpf" value="{{ old('cpf') }}" placeholder=" CPF">
+                            Caso você não possua um, deixe em branco
+                        </div>
+                    </div>
+                    <br>
+                    <div class="col-md-12 col-md-offset-1">
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <i class="material-icons">insert_invitation</i>
+                            </span>
+                            <input type="text" id="exemplo" class="form-control">
+                        </div>
+                    </div>
+                    <br>
+                    <div class="col-md-12 col-md-offset-1">
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <i class="material-icons">email</i>
+                            </span>
+                            <input type="email" id="email" class="form-control" name="email" value="{{ old('email') }}" placeholder=" Email">
+                        </div>
+                    </div>
+                    <br>
+                    <div class="col-md-12 col-md-offset-1">
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <i class="material-icons">lock_outline</i>
+                            </span>
+                            <input type="password" id="password" class="form-control" name="password" value="{{ old('password') }}" placeholder=" Senha">
+                        </div>
+                    </div>
+                    <br>
+                    <div class="col-md-12 col-md-offset-1">
+                        <div class="input-group">
+                            <span class="input-group-addon">
+                                <i class="material-icons">done</i>
+                            </span>
+                            <input type="password" id="password_confirmation" class="form-control" name="password_confirmation" value="{{ old('password_confirmation') }}" placeholder=" Confirme a senha">
+                        </div>
+                    </div>
+                    <br>
+                    <div class="col-lg-offset-6">
+                        <input type="submit" class="btn btn-primary" value="REGISTRAR"><br>
+                        <br>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
+<script>
+    function formatar(mascara, documento) {
+        var i = documento.value.length;
+        var saida = mascara.substring(0, 1);
+        var texto = mascara.substring(i)
+
+        if (texto.substring(0, 1) != saida) {
+            documento.value += texto.substring(0, 1);
+        }
+
+    }
+</script>
+</body>
+@endsection
+
+@section('javascript')
+
+<script>
+    $(document).ready(function () {
+        $('#exemplo').datepicker({
+            format: "DD/MM/yy",
+            language: "pt-BR",
+            minViewMode: 0;
+                    orientation: auto
+        });
+    });
+</script>
 @endsection
