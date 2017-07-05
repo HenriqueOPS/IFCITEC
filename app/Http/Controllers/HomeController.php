7de5e78770744a349e94b1f1a91d2420051d2d2f
Filipe Oliveira <filipeifrscanoas@gmail.com>
2017-07-05 16:57:48 -0300

    feat: Listagem de Projetos closes #21

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,16 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Funcao;
 
-class HomeController extends Controller
-{
+class HomeController extends Controller {
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         $this->middleware('auth');
     }
 
@@ -21,8 +22,23 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        return view('home');
+    public function index() {
+        $projetos = (Auth::user()->projetos);
+        $funcoes = $this->groupProjetosPorFuncao($projetos);
+        return view('home')->withFuncoes($funcoes);
     }
+
+    protected function groupProjetosPorFuncao($projetos) {
+        $funcoes = Funcao::all();
+        $projetosAgrupados = array();
+        foreach ($funcoes as $funcao) {
+            foreach ($projetos as $projeto) {
+                if ($projeto->pivot->funcao_id === $funcao->id) {
+                    $projetosAgrupados[$funcao->funcao][] = $projeto;
+                }
+            }
+        }
+        return collect($projetosAgrupados);
+    }
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,23 @@
             <a href="{{route('projeto.create')}}" class="btn btn-success">
                 Novo Projeto
             </a>
-            <br>
-            dasduhasd<br>
-            asdasd<br>
         </div>
-        <div class="col-md-9">
+        <div class="col-md-9 main main-raised">
+            <div class="list-projects">
+                @foreach($funcoes as $funcao => $projetos)
+                <span class="function">{{$funcao}}</span>
+                    @foreach($projetos as $projeto)
+                    <div class="project">
+                        <div class="project-title">
+                            <span><a href="{{route('projeto.show', ['projeto' => $projeto->id])}}">{{$projeto->titulo}}</a></span>
+                        </div>
+                        <div class="project-info">
+                            Integrantes: <span class="label label-info">Não Revisada</span>
+                        </div>
+                    </div>
+                    @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
 </div>
