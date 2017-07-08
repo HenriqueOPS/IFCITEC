e2763e70b648342062da87dfe34f6b64685530d1
Vinícius Raupp <vinicius.alves@canoas.ifrs.edu.br>
2017-07-08 01:54:54 +0000

    Revert "Merge branch 'docker-and-pipeline' into 'ifcitec2017'"
    This reverts commit 09b0dcdad12cb533a942d62ffc0c8d56b3c95b39

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,30 @@
 @extends('layouts.app')
+
 @section('content')
-<body style="background-image: url(https://scontent.fpoa1-1.fna.fbcdn.net/v/t35.0-12/19250192_943481775794342_1136829307_o.png?oh=7b0380547fb3f88dd318ac6e4055213e&oe=5947BF13); background-position: absolute; background-size: cover">
-    <br><br>
-    <div class="container" style="width: 600px; height: 350px; background-color: #eeeeee; border-radius: 10px 20px;">
-        <div class="row">
-            <div class="col-md-12 col-md-offset-4">
-                <h2>Voluntário</h2><br><br>
+<div class="container">
+    <div class="row">
+        <div class="col-md-3" >
+            <a href="{{route('projeto.create')}}" class="btn btn-success">
+                Novo Projeto
+            </a>
+        </div>
+        <div class="col-md-9 main main-raised">
+            <div class="list-projects">
+                @foreach($funcoes as $funcao => $projetos)
+                <span class="function">{{$funcao}}</span>
+                    @foreach($projetos as $projeto)
+                    <div class="project">
+                        <div class="project-title">
+                            <span><a href="{{route('projeto.show', ['projeto' => $projeto->id])}}">{{$projeto->titulo}}</a></span>
+                        </div>
+                        <div class="project-info">
+                            Integrantes: <span class="label label-info">Não Revisada</span>
+                        </div>
+                    </div>
+                    @endforeach
+                @endforeach
             </div>
-            <form method="POST" action="{{url('/cadastraVoluntario')}}">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <div class="form-group">
-                        <center> <button class="btn">
-                            <i class="material-icons">directions_run</i> Quero Ser Voluntário
-                        </button></center>
-                </div>
-            </form>
         </div>
     </div>
-</body>
+</div>
 @endsection
