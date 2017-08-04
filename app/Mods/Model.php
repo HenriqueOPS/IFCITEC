b4d0645577dcd8c135a187a8e4759daea7e4dfd1
Filipe Oliveira <filipeifrscanoas@gmail.com>
2017-08-03 23:47:33 -0300

    feat: Bloqueando visualização de projeto nao vinculado

--- app/Mods/Model.php
+++ app/Mods/Model.php
@@ -3,11 +3,8 @@
 namespace App\Mods;
 
 use Illuminate\Database\Eloquent\Model as EloquentModel;
-use App\Traits\SchemaObservable as SchemaObservable;
 
 class Model extends EloquentModel {
-
-    use SchemaObservable;
 
     public function getTable() {
         $schema = isset($this->schema) ? $this->schema : env('DB_SCHEMA');
