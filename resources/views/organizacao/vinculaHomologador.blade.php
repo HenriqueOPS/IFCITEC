5678e3632070a7713d213b2cd32bde5da8d323d2
Guilherme Prezzi <guilhermeprezzi1997@gmail.com>
2017-09-03 18:50:33 -0300

    feat(vinculaHomologador): Init de comportamentos básicos da seção.

--- resources/views/organizacao/vinculaHomologador.blade.php
+++ resources/views/organizacao/vinculaHomologador.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 
-
 @section('content')
 <div class="container">
     <div class="row">
@@ -13,17 +12,54 @@
                 </div>
                 <div class="row">
                     <div id="projeto-show">
-                        <div class="col-md-3 col-md-offset-1" style="border: 1px solid red;">
-                           <!-- CARDS DE HOMOLOGADORES AQUI -->
-                            <br>
-                            CARDS DE HOMOLOGADORES AQUI
-                            <br>
-                            <br>
+                        <div class="col-md-3 col-md-offset-1">
+                           <div id="list" class="drop-area">
+
+                               <div class="card no-select" data-id="1">
+                                    <div class="content">
+                                        <h5 class="font-weight-bold">
+                                            <b><i class="material-icons">person</i></b>
+                                            <b>Filipe Oliveira</b>
+                                        </h5>
+                                        <div>
+                                            <div>
+                                                <b><i class="material-icons small">book</i></b>
+                                                Professor de Física
+                                            </div>
+                                            <div>
+                                                <b><i class="material-icons small">settings</i></b>
+                                                5 Projetos
+                                            </div>
+                                        </div>
+                                    </div>
+                               </div>
+
+
+
+                                <div class="card no-select" data-id="2">
+                                    <div class="content">
+                                        <h5 class="font-weight-bold">
+                                            <b><i class="material-icons">person</i></b>
+                                            <b>Guilherme Prezzi</b>
+                                        </h5>
+                                        <div>
+                                            <div>
+                                                <b><i class="material-icons small">book</i></b>
+                                                Professor de Química
+                                            </div>
+                                            <div>
+                                                <b><i class="material-icons small">settings</i></b>
+                                                4 Projetos
+                                            </div>
+                                        </div>
+                                    </div>
+                               </div>
+
+                           </div>
                         </div>
-                        <div class="col-md-4" style="border: 1px solid blue">
-                            Area do ARRASTA
-                            <br>
-                            <form method="POST" action="#">
+                        <div class="col-md-4">
+                            <div id="selected" class="drop-area"></div>
+                            <form method="POST" class="text-center" action="#">
                                 <input type="hidden" id="homologadorID" name="homologadorID" value="">
                                 <input type="hidden" id="projetoID" name="projetoID" value="2">
                                 <input type="submit" class="btn btn-success" value="SALVAR">
@@ -56,8 +92,39 @@
 @endsection
 
 @section('js')
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/dragula/3.7.2/dragula.min.js"></script>
     <script type="text/javascript">
-        
+        var cardsList = document.querySelector('#list');
+        var selectedCardsList = document.querySelector('#selected');
+
+        var formHomologadorID = document.querySelector('#homologadorID');
+        formHomologadorID.value = '';
+
+        var drag = dragula([cardsList, selectedCardsList]);
+
+        drag.on('drag', function() {
+            cardsList.classList.add('active');
+            selectedCardsList.classList.add('active');
+        });
+
+        drag.on('dragend', function() {
+            cardsList.classList.remove('active');
+            selectedCardsList.classList.remove('active');
+        });
+
+        drag.on('drop', function(el, target) {
+            var id = el.getAttribute('data-id');
+            if (target.id === 'selected') {
+                var value = formHomologadorID.value;
+
+                if (value.length > 0) value = value.concat(',');
+                formHomologadorID.value = value.concat(id);
+            } else {
+                var values = formHomologadorID.value.split(',');
+                values.splice(values.indexOf(id), 1);
+                formHomologadorID.value = values.join(',');
+            }
+        });
     </script>
 @endsection
 
