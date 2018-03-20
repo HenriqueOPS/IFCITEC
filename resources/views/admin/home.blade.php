9892455fadfbd55776915540af99f5daae337822
rafaellabueno <rafaellasbueno@gmail.com>
2018-03-20 13:58:47 -0300

    feat: CRUD de nível

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -131,7 +131,85 @@
 
                     @endforeach
 
-                    
+                    </tbody>
+
+                    <thead id="2">
+                    <div id="2">
+                        <div class="col-md-3">
+                            <a href="{{ route('cadastroNivel') }}" class="btn btn-primary btn-round">
+                                <i class="material-icons">add</i> Adicionar Nível
+                            </a>
+                        </div>
+                    </div>
+                    <tr>
+                        <th class="text-center">#</th>
+                        <th>Nível</th>
+                        <th>Descrição</th>
+                        <th class="text-right">Ações</th>
+                    </tr>
+                    </thead>
+
+                    <tbody id="2">
+
+                    @foreach($niveis as $i => $nivel)
+
+                        <tr>
+                            <td class="text-center">{{ $i }}</td>
+                            <td>{{ $nivel->nivel }}</td>
+                            <td>{{ $nivel->descricao }}</td>
+
+                            <td class="td-actions text-right">
+
+                                <a href="javascript:void(0);" class="modalNivel" id-nivel="{{ $nivel->id }}"><i class="material-icons blue-icon">remove_red_eye</i></a>
+
+                                <a href="{{ route('nivel', $nivel->id) }}"><i class="material-icons">edit</i></a>
+
+                                <a href="javascript:void(0);" class="exclusaoNivel" id-nivel="{{ $nivel->id }}"><i class="material-icons blue-icon">close</i></a>
+                            </td>
+                        <tr>
+
+                    @endforeach
+
+                    </tbody>
+
+                    <thead id="3">
+                    <div id="3">
+                        <div class="col-md-3">
+                            <a href="{{ route('cadastroArea') }}" class="btn btn-primary btn-round">
+                                <i class="material-icons">add</i> Adicionar Área do Conhecimento
+                            </a>
+                        </div>
+                    </div>
+                    <tr>
+                        <th class="text-center">#</th>
+                        <th>Área do Conhecimento</th>
+                        <th>Descrição</th>
+                        <th class="text-right">Ações</th>
+                    </tr>
+                    </thead>
+
+                    <tbody id="2">
+
+                    @foreach($areas as $i => $area)
+
+                        <tr>
+                            <td class="text-center">{{ $i }}</td>
+                            <td>{{ $area->area_conhecimento }}</td>
+                            <td>{{ $area->descricao }}</td>
+
+                            <td class="td-actions text-right">
+
+                                <a href="javascript:void(0);" class="modalArea" id-area="{{ $area->id }}"><i class="material-icons blue-icon">remove_red_eye</i></a>
+
+                                <a href="{{ route('area', $area->id) }}"><i class="material-icons">edit</i></a>
+
+                                <a href="javascript:void(0);" class="exclusao" id-area=""><i class="material-icons blue-icon">close</i></a>
+                            </td>
+                        <tr>
+
+                    @endforeach
+
+                    </tbody>
 
                 </table>
             </div>
@@ -139,7 +217,8 @@
     </div>
 </div>
 
-<!-- Modal Delete -->
+
+<!-- Modal Delete Escola -->
 <div id="ModalDelete" class="modal fade bd-example-modal-lg" role="dialog1" aria-labelledby="ModalDelete">
     <div class="modal-dialog" role="document1">
         <div class="modal-content">
@@ -149,6 +228,75 @@
 
             <div class="modal-body">
                 <span>Para deletar a escola, confirme sua senha.</span>
+                <div class="input-group">
+                    <span class="input-group-addon">
+                        <i class="material-icons">lock_outline</i>
+                    </span>
+                    <input type="password" placeholder="Senha..." class="form-control" id="password" name="password" required>              
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary excluir" data-dismiss="modal">Excluir</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Fim Modal -->
+
+<!-- Modal Nível -->
+<div id="nivelModal" class="modal fade bd-example-modal-lg" role="dialog2" aria-labelledby="nivelModal">
+  <div class="modal-dialog" role="document2">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="nivelModal"></h5>
+        </div>
+        <div class="modal-body">
+
+            <div class="input-group">
+                <span class="input-group-addon">
+                    <i class="material-icons">assignment</i>
+                </span>
+                <div class="form-group label-floating">
+                    <span id="max_chModal"></span>
+                </div>
+            </div>
+
+            <div class="input-group">
+                <span class="input-group-addon">
+                    <i class="material-icons">mail</i>
+                </span>
+                <div class="form-group label-floating">
+                    <span id="min_chModal"></span>
+                </div>
+            </div>
+
+            <div class="input-group">
+                <span class="input-group-addon">
+                    <i class="material-icons">phone</i>
+                </span>
+                <div class="form-group label-floating">
+                    <span id="descricaoModal"></span>
+                </div>
+            </div>
+
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
+        </div>
+    </div>
+</div>
+<!-- Fim Modal Nível -->
+
+<!-- Modal Delete Nível -->
+<div id="ModalDeleteNivel" class="modal fade bd-example-modal-lg" role="dialog3" aria-labelledby="ModalDeleteNivel">
+    <div class="modal-dialog" role="document3">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Deletar Nível</h5>
+            </div>
+
+            <div class="modal-body">
+                <span>Para deletar o nível, confirme sua senha.</span>
                 <div class="input-group">
                     <span class="input-group-addon">
                         <i class="material-icons">lock_outline</i>
@@ -228,6 +376,7 @@
 
 
 
+
 @endsection
 
 @section('js')
@@ -262,6 +411,32 @@ $('.modalEscola').click(function(){
 
         //abre a modal
         $("#myModal").modal();
+
+    });
+
+})
+</script>
+
+<script type="application/javascript">
+$('.modalNivel').click(function(){
+
+    var idNivel = $(this).attr('id-nivel');
+
+    //monta a url de consulta
+    var urlConsulta = './nivel/dados-nivel/'+idNivel;
+    //faz a consulta via Ajax
+    $.get(urlConsulta, function (res){
+
+        console.log(res);
+
+        //altera o DOM
+        $("#nivelModal").html(res.dados.nivel);
+        $("#max_chModal").html(res.dados.max_ch);
+        $("#min_chModal").html(res.dados.min_ch);
+        $("#descricaoModal").html(res.dados.descricao);
+
+        //abre a modal
+        $("#nivelModal").modal();
 
     });
 
@@ -319,6 +494,27 @@ $('.exclusao').click(function(){
         $.get(urlConsulta, function (res){
             if(res == 'true'){
                 alert("Escola excluida");
+                location.href = './administrador';
+            }else{
+                alert("Senha incorreta");
+            }
+
+        });
+    });
+
+});
+</script>
+<script type="application/javascript">
+$('.exclusaoNivel').click(function(){
+    var idNivel= $(this).attr('id-nivel');
+
+    $("#ModalDeleteNivel").modal();
+
+    $('.excluir').click(function(){
+        var urlConsulta = './nivel/exclui-nivel/'+idNivel+'/'+$('#password').val();
+        $.get(urlConsulta, function (res){
+            if(res == 'true'){
+                alert("Nível excluido");
                 location.href = './administrador';
             }else{
                 alert("Senha incorreta");
