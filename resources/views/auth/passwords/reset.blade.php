ebc4c905f7d00c4b2b64b0e8a35da432fdf427fb
muchsousa <muchsousa@hotmail.com>
2018-06-28 19:41:15 -0300

    Fix: password resets

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,11 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container" style="margin-top: 100px; ">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+
+				<div class="content text-center box-logo">
+					<a class="btn btn-simple btn-just-icon">
+						<img src="{{ asset('img/logonormal.png') }}" title="IFCITEC" height="110" />
+					</a>
+				</div>
+
+				<h3 class="text-center" style="width: 90%; margin: auto; border-bottom: 1px solid #ccc;">Redefinir Senha</h3>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -20,7 +27,7 @@
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address
+                            <label for="email" class="col-md-4 control-label">E-Mail
                             </label>
 
                             <div class="col-md-6">
@@ -34,28 +41,28 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                        <div class="form-group{{ $errors->has('senha') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Senha</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" class="form-control" name="senha" required>
 
-                                @if ($errors->has('password'))
+                                @if ($errors->has('senha'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
+                                        <strong>{{ $errors->first('senha') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                        <div class="form-group{{ $errors->has('senha_confirmation') ? ' has-error' : '' }}">
+                            <label for="password-confirm" class="col-md-4 control-label">Confirmar Senha</label>
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                <input id="password-confirm" type="password" class="form-control" name="senha_confirmation" required>
 
-                                @if ($errors->has('password_confirmation'))
+                                @if ($errors->has('senha_confirmation'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                        <strong>{{ $errors->first('senha_confirmation') }}</strong>
                                     </span>
                                 @endif
                             </div>
@@ -64,7 +71,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Reset Password
+									Redefinir Senha
                                 </button>
                             </div>
                         </div>
