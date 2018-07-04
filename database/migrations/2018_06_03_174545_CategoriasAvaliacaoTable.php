2897262188083e7ce57e01327b849f4cfa8e24a2
muchsousa <muchsousa@hotmail.com>
2018-07-04 18:55:17 -0300

    Fix: rename CategoriaAvaliacaoTable to CategoriasAvaliacaoTable

--- database/migrations/2018_06_03_174545_CategoriasAvaliacaoTable.php
+++ database/migrations/2018_06_03_174545_CategoriasAvaliacaoTable.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CategoriaAvaliacaoTable extends Migration
+class CategoriasAvaliacaoTable extends Migration
 {
     /**
      * Run the migrations.
