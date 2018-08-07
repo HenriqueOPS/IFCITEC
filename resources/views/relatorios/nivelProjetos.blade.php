12742f2b3a2062059f18b1c44c3528ecb16ff7c9
rafaellabueno <rafaellasbueno@gmail.com>
2018-08-07 12:21:19 -0300

    fix: relatorios

--- resources/views/relatorios/nivelProjetos.blade.php
+++ resources/views/relatorios/nivelProjetos.blade.php
@@ -3,12 +3,13 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <h2 style="margin-top: 20px; margin-left: 200px;">RELATÓRIO DE PROJETOS DO NÍVEL {{$nivel->nivel}}</h2>
+        <h2 style="margin-top: 5mm; margin-left: 50mm;">RELATÓRIO DE PROJETOS DO NÍVEL </h2>
+        <h2 style="text-align: center;">{{$nivel->nivel}}</h2>
 
 
-        <h5 style="margin-top: 30px; margin-left: 20px;">Número de projetos: {{$numeroProjetos}}</h5>
+        <h5 style="margin-top: 10mm; margin-left: 20mm;">Número de projetos: {{$numeroProjetos}}</h5>
 
-        <table style="margin-left: 25px; margin-top: 50px; width: 100%;">
+        <table style="margin-left: 25mm; margin-top: 10mm; width: 100%;">
         	<thead">
         		<tr>
             <th>#</th>
