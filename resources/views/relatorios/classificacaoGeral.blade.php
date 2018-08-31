3082850e4b0e9a4809a3137633a560d8862f71c9
rafaellabueno <rafaellasbueno@gmail.com>
2018-08-31 19:11:03 -0300

    fix: relatorios

--- resources/views/relatorios/classificacaoGeral.blade.php
+++ resources/views/relatorios/classificacaoGeral.blade.php
@@ -4,13 +4,12 @@
 <div class="container">
     <div class="row">
         <h2 style="text-align: center; font-size: 25px;">RELATÓRIO DA CLASSIFICAÇÃO GERAL DA {{\App\Edicao::numeroEdicao(\App\Edicao::find(\App\Edicao::getEdicaoId())->ano)}} IFCITEC</h2>
-
-      <p style="text-align: center;"><b>{{$area->niveis->nivel}} : {{$area->area_conhecimento}}</b></p>
+		
       <table class="bordered striped centered" style="width: 100%">
         	<thead>
         	<tr>
     				<th>Projeto</th>
-    				<th>Nota Final</th> 
+    				<th>Nota Final</th>
   				</tr>
         	</thead>
   			<tbody>
@@ -20,8 +19,8 @@
     				<td><a style="color: #000;">{{$projeto->nota_avaliacao}}</a></td>
   				</tr>
           @endforeach
-  			</tbody>	
-		</table> 
+  			</tbody>
+		</table>
     <br><br>
 	</div>
 </div>
