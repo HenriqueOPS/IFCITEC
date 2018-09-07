3679a97ee6aefe65a6c95f34fa1af0c04894f207
rafaellabueno <rafaellasbueno@gmail.com>
2018-09-06 22:05:06 -0300

    fix: relatorios

--- resources/views/admin/gerarLocalizacaoProjetos.blade.php
+++ resources/views/admin/gerarLocalizacaoProjetos.blade.php
@@ -22,7 +22,9 @@
                     {{ csrf_field() }}
 
 
-                    <div class="row">
+                    <div class="row" id="selects">
+                        <div id="displayOriginal">
+                           <div>
                             <div class="col-md-10 col-md-offset-1 col-xs-9 col-xs-offset-1">
                                 <div class="form-group label-floating">
                                     <label class="control-label">Bloco</label>
@@ -47,6 +49,14 @@
                                     <input type="text" class="form-control" name="num" required>
                                 </div>
                             </div>
+
+                            <div class="col-md-12 col-md-offset-1" class="col-xs-6">
+                                <a onclick="addBloco()" class="btn btn-success btn-fab btn-fab-mini btn-round" role="button">
+                                    <i class="material-icons">add</i>
+                                </a>
+                            </div>
+                           </div>
+                          </div>
                         </div>
                         <div class="row">
                             <div class="col-md-6 col-md-offset-3 text-center">
@@ -63,4 +73,36 @@
 </div>
 </div>
 
+@endsection
+
+@section('js')
+<script>
+var total = 0;
+
+    function deletaBloco(id) {
+        console.log($('#' + id));
+        $('#' + id).remove();
+    }
+
+    function addBloco() {
+        var newSelect = $("#displayOriginal");
+        newSelect = newSelect.clone();
+        id =  'select-' + total++;
+        newSelect.find('div').attr("id", id);
+        newSelect.find('div').css('display', 'block');
+
+        newSelect.find('div input').attr('name', 'bloco[]');
+
+        button = '<a class="btn btn-danger btn-fab btn-fab-mini btn-round" role="button" onclick="deletaBloco(\'' + id + '\')" style="color: red;"><i class="material-icons">remove</i></a>'; 
+
+        console.log(button);
+        newSelect.find('a').append(button);
+
+        $("#selects").append(newSelect.html());
+
+        
+
+        console.log(newSelect.find('div select'));
+}
+</script>
 @endsection
