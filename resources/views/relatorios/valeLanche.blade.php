5f268810cf1e521a83dc4868267d4599c645dd97
rafaellabueno <rafaellasbueno@gmail.com>
2018-09-13 19:59:50 -0300

    fix: relatorios

--- resources/views/relatorios/valeLanche.blade.php
+++ resources/views/relatorios/valeLanche.blade.php
@@ -5,26 +5,36 @@
 <style>
 
 	@import "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700";
-	@media all {
 	@page{
-		margin: 2mm;
-		width: 200mm;
-		height: 290mm;
+		size: A4 portrait;
+		width: 21cm;
+		height: 29.7cm;
+		margin: 0.5cm;
 	}
+	@media all {
 	*{
 		margin:0;
 		padding: 0;
 		border: 0;
-		-webkit-print-color-adjust: exact !important;   /* Chrome, Safari */
-		color-adjust: exact !important;                 /*Firefox*/
+		
 	}
-	body{font-family: "Roboto", "Helvetica", "Arial", sans-serif;
-		background: url("{{ asset('img/lanche.png') }}");
+	body{font-family: "Roboto", "Helvetica", "Arial", sans-serif;}
+	.imagem{
+		width: 350px;
+		border: 1pt solid #000;
+		display: inline-flex;
+		padding: 1mm;
+		margin: 1mm;
+		overflow: hidden;
 	}
 	}
+
 </style>
 </head>
 <body>
-
+@for ($i = 1; $i <= $cont; $i++)
+		<img src="{{ asset('img/lanche.png') }}" class="imagem">
+	
+@endfor
 </body>
 </html>
