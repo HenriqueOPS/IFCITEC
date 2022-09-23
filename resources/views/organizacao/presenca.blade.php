06149703fb122743064a685608c77c945a69d4b0
GatoImorrivel <guilhermevianagui23@gmail.com>
2022-09-23 12:26:57 -0300

    fix: typo

--- resources/views/organizacao/presenca.blade.php
+++ resources/views/organizacao/presenca.blade.php
@@ -107,7 +107,7 @@
 
     Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
-            if(cmaeras.length > 1)
+            if(cameras.length > 1)
                 scanner.start(cameras[1]);
             else
                 scanner.start(cameras[0]);
