3be603946243e0beb517d1fca94e0992caee7348
GatoImorrivel <guilhermevianagui23@gmail.com>
2022-10-13 11:32:35 -0300

    feat: add summernote

--- resources/views/admin/gerenMsg/email.blade.php
+++ resources/views/admin/gerenMsg/email.blade.php
@@ -10,14 +10,28 @@
             @include('partials.admin.navbar')
         </div>
     </div>
-    <br><br>
     <div class="container">
-        Oi
+        <div class="card-body">
+            <form method=”POST” action="">
+                <div class="form-group">
+                    <textarea class="form-control" name="summernote" id="summernote"></textarea>
+                </div>
+                <button type=”submit” class="btn btn-danger btn-block">Save</button>
+            </form>
+        </div>
     </div>
 @endsection
 
 @section('js')
-<script>
-    document.getElementById('nav-mensagens').classList.add('active');
-</script>
+    <!-- summernote css/js -->
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
+
+    <script>
+        document.getElementById('nav-mensagens').classList.add('active');
+
+        $(document).ready(function() {
+            $('#summernote').summernote();
+        });
+    </script>
 @endsection
