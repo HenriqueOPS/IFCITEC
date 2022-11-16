8ec4dd655c3308305b0cb85928386af1cf578c7d
GatoImorrivel <guilhermevianagui23@gmail.com>
2022-11-16 10:30:02 -0300

    removendo rota de teste de email

--- routes/web.php
+++ routes/web.php
@@ -228,18 +228,6 @@ Route::group(['middleware' => ['IsAdministrador']], function () {
 	Route::post('/mensagens/salvar', 'GerenMsgController@save')->name('mensagens.save'); // Ajax
 	Route::post('/administrador/mensagens/create/{nome}/{tipo}', 'GerenMsgController@create')->name('mensagens.create'); // Ajax
 	Route::post('/administrador/mensagens/deletar/{id}', 'GerenMsgController@delete')->name('mensagens.delete'); // Ajax
-	Route::get('/administrador/mensagens/tester', function () {
-		dispatch(
-			new MailBaseJob(
-				'guilhermevianagui23@gmail.com',
-				'Projeto Homologado',
-				[
-					'nome' => 'Guilherme',
-					'titulo' => 'Bolas'
-				]
-			)
-		);
-	});
 
 	// TODO: refatorar
 	Route::get('/administrador/nota-revisao/projeto/{projeto}', 'AdminController@notaRevisao')->name('notaRevisao');
