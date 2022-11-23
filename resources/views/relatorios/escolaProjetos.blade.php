20b4da381877d67c6bf31c73cc6e3700a0270661
GatoImorrivel <guilhermevianagui23@gmail.com>
2022-11-23 11:40:15 -0300

    fix: aspas perdida relatorio projetos por escola

--- resources/views/relatorios/escolaProjetos.blade.php
+++ resources/views/relatorios/escolaProjetos.blade.php
@@ -17,23 +17,23 @@
                 @foreach ($situacoes as $situacao)
                     @php($cont = 0)
                     <table class="bordered striped centered" style="width: 100%;">
-                        <thead">
+                        <thead>
                             <tr>
                                 <th>#</th>
                                 <th>Título</th>
                             </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($projetos as $projeto)
-                                    @if ($projeto->situacao == $situacao->situacao)
-                                        @php($cont += 1)
-                                        <tr>
-                                            <td><a style="color: #000;">{{ $cont }}</a></td>
-                                            <td><a style="color: #000;">{{ $projeto->titulo }}</a></td>
-                                        </tr>
-                                    @endif
-                                @endforeach
-                            </tbody>
+                        </thead>
+                        <tbody>
+                            @foreach ($projetos as $projeto)
+                                @if ($projeto->situacao == $situacao->situacao)
+                                    @php($cont += 1)
+                                    <tr>
+                                        <td><a style="color: #000;">{{ $cont }}</a></td>
+                                        <td><a style="color: #000;">{{ $projeto->titulo }}</a></td>
+                                    </tr>
+                                @endif
+                            @endforeach
+                        </tbody>
                     </table>
                     <h5 style="font-size: 20px; margin-bottom: 25pt;">Numero de {{ $situacao->situacao }}:
                         {{ $cont }}</h5>
