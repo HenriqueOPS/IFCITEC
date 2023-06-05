12da59fff3caf9be24d9b4c6ce652a96a2bd41c6
Henrique <henriquepalmaifrs@gmail.com>
2023-06-05 18:11:41 -0300

    Fix(cor)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider {
 
@@ -12,6 +14,8 @@ class AppServiceProvider extends ServiceProvider {
      * @return void
      */
     public function boot() {
+        $cor = DB::table('mensagem')->where('nome','=','cor_navbar')->get();
+        view()->share('cor', $cor[0]->conteudo);
 
         Schema::defaultStringLength(191);
         if (env('APP_ENV') !== 'local') {
