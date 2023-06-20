3bbbc56857bf87c96042413e5ca1b3db8c60d438
Henrique <henriquepalmaifrs@gmail.com>
2023-06-20 20:42:18 -0300

    Refact(configuracoes)

--- resources/views/admin/configuracoes/configuracoes.blade.php
+++ resources/views/admin/configuracoes/configuracoes.blade.php
@@ -9,17 +9,18 @@
 
 		@include('partials.admin.navbar')
 	</div>
-    <div class="container" style="margin-top: 30px;">
+    <div class="container">
         <div class="row">
             <div class="col-md-12 main main-raised" >
                 <div class="text-center">
                    <h2>Configurações</h2>
                 </div>
-
-                <form method="POST" action="{{ route('admin.background') }}" enctype="multipart/form-data" style="padding:70px ;">
-                                    {{ csrf_field() }}
+                
+                <div>
+                    <form method="POST" action="{{ route('admin.background') }}" enctype="multipart/form-data">
+                                {{ csrf_field() }}
                                     <label for="image">Selecione a Imagem, deverá ser do tipo png</label>
-                                    <input type="file" id="File" name="image" class="form-controll-file"/>
+                                    <input type="file" id="formFile" name="image" class="form-controll-file"/>
                                     <input type="radio" value="background" id="background-radio" name="imagem"/>
                                     <label for="background-radio">Tela da Plataforma</label>
                                     <input type="radio" value="teladelogin" id="teladelogin-radio" name="imagem"/>
@@ -27,16 +28,18 @@
                                     <input type="radio" value="ifcitecheader" id="ifcitecheader-radio" name="imagem"/>
                                     <label for="ifcitecheader-radio">Cabeçalho dos relátorios</label>
                                     <input type="radio" value="logo" id="logo-radio" name="imagem"/>
-                                    <label for="logo-radio">Logotipo</label><br>
+                                    <label for="logo-radio">Logo da navbar</label>
+                                    <input type="radio" value="logonormal" id="logo-radio" name="imagem"/>
+                                    <label for="logo-radio">Logo da tela de login</label>
                                     <button type="submit" class="btn btn-primary" >mudar imagem</button>
-                </form>
-                <form method="POST" action="{{ route('admin.navbar')}}"style="padding:70px ;">
-                {{ csrf_field() }}
-                <label for="favcolor">Selecione a cor:</label>
-                <input type="color" id="favcolor" name="cor" value="#ff0000"><br><br>
-                <button type="submit" class="btn btn-primary"  >Mudar cor da NavBar</button>
-                </form>
-              
+                    </form>
+                    <form method="POST" action="{{ route('admin.navbar')}}">
+                        {{ csrf_field() }}
+                        <label for="favcolor">Selecione a cor:</label>
+                        <input type="color" id="favcolor" name="cor" value="#ff0000"><br><br>
+                        <button type="submit" class="btn btn-primary"  >Mudar cor da NavBar</button>
+                    </form>
+                </div>
 
             </div>
            
