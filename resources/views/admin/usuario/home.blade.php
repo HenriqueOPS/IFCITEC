2f11526face54ee360a5ab8d014c3078e40a4d4a
Henrique <henriquepalmaifrs@gmail.com>
2023-08-24 11:43:53 -0300

    Feat(FiltrarOcultos)

--- resources/views/admin/usuario/home.blade.php
+++ resources/views/admin/usuario/home.blade.php
@@ -34,44 +34,63 @@ function oculto($id,$estado){
 	</div>
 </div>
 <br><br>
+<script>
+    function filtrarOcultos() {
+        const ocultosCheckbox = document.getElementById('ocultosCheckbox');
+        const registros = document.querySelectorAll('.oculto');
+
+        registros.forEach(registro => {
+            registro.style.display = ocultosCheckbox.checked ? 'none' : 'table-row';
+        });
+    }
+</script>
+
+
+
 <div class="container">
     <div class="row">
         <div class="col-md-12 main main-raised">
             <div class="list-projects">
-              <table class="table">
-                <thead id="5">
-                    <div id="5">
-                        <tr>
-                            <th class="text-center">#</th>
-                            <th>Usuários</th>
-                            <th>Ocultar</th>
-                            <th>E-mail</th>
-                            <th class="text-right">Ações</th>
-                        </tr>
-                    </div>
-                    </thead>
+            <div class="form-check">
+    <input class="form-check-input" type="checkbox" id="ocultosCheckbox" onClick="filtrarOcultos()">
+    <label class="form-check-label" for="ocultosCheckbox">
+        Mostrar Ocultos
+    </label>
+</div>
 
-                        <tbody id="5">
-                                                        @foreach($usuarios as $key=>$usuario)
-                                <tr>
-                                <td class="text-center">{{$key + 1}}</td>
-                                <td>
-                                <input type="checkbox" class="custom-control-input" id="customSwitch{{$usuario->id}}" onclick="oculto({{$usuario->id}}, {{$usuario->oculto}})" {{$usuario->oculto ? 'checked' : ''}}>
-  
-                                <label>Ocultar</label>
-                                </td>
-                                <td>{{$usuario->nome}}</td>
-                                <td>{{$usuario->email}}</td>
-                                <td class="text-right">
-                                <a href="{{route('editarUsuario',$usuario->id)}}"><i class="material-icons">edit</i></a>
-                                <a href="{{route('editarFuncaoUsuario',$usuario->id)}}"><i class="material-icons">person_add</i></a>
-                
-                                <a href="{{route('ocultarUsuario',$usuario->id)}}"><i class="material-icons">block</i></a>
-                              </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                    </table>
+<table class="table">
+    <thead>
+        <tr>
+            <th class="text-center">#</th>
+            <th>Usuários</th>
+            <th>Oculto</th>
+            <th>E-mail</th>
+            <th class="text-right">Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($usuarios as $key => $usuario)
+        <tr id="registro{{$usuario->id}}" class="{{$usuario->oculto ? '' : 'oculto'}}">
+            <td class="text-center">{{$key + 1}}</td>
+            <td>{{$usuario->nome}}</td>
+            <td>
+                <div class="custom-control custom-switch">
+                    <input type="checkbox" class="custom-control-input" id="customSwitch{{$usuario->id}}" onclick="oculto({{$usuario->id}}, {{$usuario->oculto}})" {{$usuario->oculto ? 'checked' : ''}}>
+                    <label class="custom-control-label" for="customSwitch{{$usuario->id}}">Oculto</label>
+                </div>
+            </td>
+            <td>{{$usuario->email}}</td>
+            <td class="text-right">
+                <a href="{{route('editarUsuario',$usuario->id)}}"><i class="material-icons">edit</i></a>
+                <a href="{{route('editarFuncaoUsuario',$usuario->id)}}"><i class="material-icons">person_add</i></a>
+                <a href="{{route('ocultarUsuario',$usuario->id)}}"><i class="material-icons">block</i></a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+
                 </div>
             </div>
         </div>
