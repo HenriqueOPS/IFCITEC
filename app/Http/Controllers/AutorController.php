64d10e730c581f052ad914db4b68d8e8e922c077
Henrique <henriquepalmaifrs@gmail.com>
2023-08-31 18:39:52 -0300

    Feat(NotasHomologaçãoAvaliacao)

--- app/Http/Controllers/AutorController.php
+++ app/Http/Controllers/AutorController.php
@@ -39,9 +39,12 @@ class AutorController extends Controller {
 			->where('projeto.edicao_id', '=', Edicao::getEdicaoId())
 			->where('funcao_id', '=', 7) // Orientador
 			->get();
-			$DataFechamento = DB::table('edicao')
+			$DataFechamentoHom = DB::table('edicao')
 			->where('id',Edicao::getEdicaoId())
 			->pluck('homologacao_fechamento');
+			$DataFechamentoAva = DB::table('edicao')
+			->where('id',Edicao::getEdicaoId())
+			->pluck('avaliacao_fechamento');
 		$projetos['coorientador'] = Projeto::select('id', 'titulo')
 			->join('escola_funcao_pessoa_projeto', 'projeto.id', '=', 'projeto_id')
 			->where('pessoa_id', '=', Auth::user()->id)
@@ -60,22 +63,27 @@ class AutorController extends Controller {
 				->where('dados_avaliacao.projeto_id',$projetos['autor'][0]->id)
 				->join('revisao','dados_avaliacao.pessoa_id','revisao.pessoa_id')
 				->where('revisao.projeto_id',$projetos['autor'][0]->id)
-				->select('valor','observacao','categoria_avaliacao','campos_avaliacao.descricao','nota_final','categoria_avaliacao.peso')
+				->select('dados_avaliacao.pessoa_id','valor','observacao','categoria_avaliacao','campos_avaliacao.descricao','nota_final','categoria_avaliacao.peso')
 				->get();		
 			$homologacao = $homologacao->groupBy('pessoa_id')->map(function ($itens) {
 				return $itens->values()->toArray();
 			})->values()->toArray();
 			$avaliacao = DB::table('formulario')
 			->where('formulario.edicao_id',Edicao::getEdicaoId())
-			->where('formulario.nivel_id',$projetos['autor'][0]->nivel_id)
+			->where('formulario.nivel_id',2)
 			->where('tipo','avaliacao')
 			->join('formulario_categoria_avaliacao','formulario.idformulario','formulario_categoria_avaliacao.formulario_idformulario')
 			->join('categoria_avaliacao','formulario_categoria_avaliacao.categoria_avaliacao_id','categoria_avaliacao.id')
 			->join('campos_avaliacao','categoria_avaliacao.id','campos_avaliacao.categoria_id')
 			->join('dados_avaliacao','dados_avaliacao.campo_id','campos_avaliacao.id')
-			->where('projeto_id',$projetos['autor'][0]->id)
-			->get();
-
+			->where('dados_avaliacao.projeto_id',605)
+			->join('revisao','dados_avaliacao.pessoa_id','revisao.pessoa_id')
+			->where('revisao.projeto_id',605)
+			->select('dados_avaliacao.pessoa_id','valor','observacao','categoria_avaliacao','campos_avaliacao.descricao','nota_final','categoria_avaliacao.peso')
+			->get();	
+			$avaliacao = $avaliacao->groupBy('pessoa_id')->map(function ($itens) {
+				return $itens->values()->toArray();
+			})->values()->toArray();
 			}else{
 				$homologacao = null;
 				$avaliacao = null;
@@ -85,7 +93,8 @@ class AutorController extends Controller {
 		return view('user.home')
 		->withProjetos($projetos)
 		->withAvaliacao($avaliacao)
-		->withData($DataFechamento[0])
+		->withDatahom($DataFechamentoHom[0])
+		->withDataava($DataFechamentoAva[0])
 		->withHomologacao($homologacao);
     }
 	public function nota($id){
