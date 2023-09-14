534687754abb020bee3a5bd48ef85fa68b98fac6
Carlos <carlos1557amd@gmail.com>
2023-09-14 20:35:13 -0300

    Refact(AdminNavbar)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,7 +93,9 @@
 
     @media (max-width: 778px){
         #barrinha{
-            overflow-x: scroll !important;
+            display:flex !important;
+            flex-direction: column !important;
+            align-items: center !important;
         }
     
     }   
