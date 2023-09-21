d1c3a482cb31e42fbeab53ea93621f7a8f186127
Henrique <henriquepalmaifrs@gmail.com>
2023-09-21 12:36:18 -0300

    Fix()

--- resources/views/organizacao/presenca.blade.php
+++ resources/views/organizacao/presenca.blade.php
@@ -108,7 +108,7 @@
     Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
             if(cameras.length > 1)
-                scanner.start(cameras[1]);
+                scanner.start(cameras[0]);
             else
                 scanner.start(cameras[0]);
         } else {
