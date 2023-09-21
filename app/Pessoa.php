7bfd1d6e5d7b8b8df454959d88abd4439901c9b4
Henrique <henriquepalmaifrs@gmail.com>
2023-09-21 08:12:49 -0300

    Fix()

--- app/Pessoa.php
+++ app/Pessoa.php
@@ -339,7 +339,7 @@ class Pessoa extends Authenticatable
     public function EcontroladorDePresenca(){
         $idfuncao = DB::table('tarefa')
         ->select('id')
-        ->where('tarefa', ' Controlador de Presença')
+        ->where('id', 17)
         ->first();
         $status = DB::table('pessoa_tarefa')
         ->where('pessoa_id',$this->id)
