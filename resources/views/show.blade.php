decb67c038057af058b03297a0ee6e0cd3751e67
Henrique <henriquepalmaifrs@gmail.com>
2023-10-19 09:40:20 -0300

    Try()

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,7 +4,7 @@
     <title>Exibição de Imagem</title>
 </head>
 <body>
-<img src="{{url(''.$caminho)}}" alt="Imagem em base64" style="width: 297mm; height: 210mm;">
+<img src="{{asset(''.$caminho)}}" alt="Imagem em base64" style="width: 297mm; height: 210mm;">
 
 
 </body>
